e99e8fc67d1170ce862751386b1b05176b0496c7
Erwin Nahue <erwinnahue@gmail.com>
2023-07-04 23:19:08 -0500

    Update Api_key

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Loader from "./components/Loader";
 import WeatherCard from "./components/WeatherCard";
 
 
-const API_KEY = import.meta.env.REACT_APP_WEATHER_API_KEY;
+const API_KEY = import.meta.env.WEATHER_API_KEY;
 function App() {
   const [coords, setCoords] = useState();
   const [weather, setWeather] = useState();
